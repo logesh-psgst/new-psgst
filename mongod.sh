7727e624a7611afbcf5437e3784568ffc1f9a4c5
logesh-psgst <103935228+logesh-psgst@users.noreply.github.com>
2022-05-10 15:56:04 +0530

    Update mongod.sh

--- mongod.sh
+++ mongod.sh
@@ -5,14 +5,14 @@
 #######################################
 
 echo ".................................INSTALLNG JAVA..................................................."
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common  # Adding the ppa repository and install openjdk in ubuntu
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update  # To get the latest package lists
 sudo apt-get install openjdk-8-jdk -y
 echo "JAVA_HOME=$(which java)" | sudo tee -a /etc/environment
 source /etc/environment
 echo $JAVA_HOME
-java -version
+java -version  # openjdk version "1.8.0_191"
 
 
 echo ".................................INSTALLNG NODEJS..................................................."
@@ -33,7 +33,7 @@ npm set registry http://npm.psgsoftwaretechnologies.com
 
 
 echo ".................................INSTALLNG NGINX..................................................."
-sudo apt-get update
+sudo apt-get update  # To get the latest package lists
 sudo apt-get install nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
@@ -45,28 +45,29 @@ git  --version
 
 
 echo ".................................INSTALLNG MONGO DB..................................................."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5  #  Adding MongoDB repository
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list  #  Createing a list file for MongoDB
+sudo apt-get update  # To get the latest package lists
+sudo apt-get install -y mongodb-org  #  Installing and Verifying MongoDB
 sudo systemctl start mongod
-sudo systemctl status mongod
-mongod –version
+sudo systemctl enable mongod  #  Enable automatically starting MongoDB when the system starts
+sudo systemctl status mongod  #  Check MongoDB status
+mongod –version  #  Check MongoDB version
 
 
 echo ".................................INSTALLNG ERLANG..................................................."
-sudo apt update
+sudo apt update  # To get the latest package lists
 sudo apt install software-properties-common apt-transport-https
 wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -
 echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-sudo apt update
+sudo apt update  # To get the latest package lists
 sudo apt install erlang
 erl -v
 
 
 echo ".................................INSTALLNG RABBIT MQ..................................................."
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | sudo bash
-sudo apt update
+sudo apt update  # To get the latest package lists
 sudo apt install rabbitmq-server
 systemctl start rabbitmq-server.service
 systemctl status rabbitmq-server.service
